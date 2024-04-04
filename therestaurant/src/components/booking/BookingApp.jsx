ce3bf63ae708e5835efb4e9be87223523f1b1354
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-04-04 17:41:11 +0200

    feat(booking): added general logic
    style: added some fundamental styling

--- therestaurant/src/components/booking/BookingApp.jsx
+++ therestaurant/src/components/booking/BookingApp.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CalendarApp } from '../calendar/CalendarApp';
 
 export const BookingApp = () => {
+  const currentDate = new Date(
+    Date.UTC(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate()
+    )
+  )
+    .toISOString()
+    .slice(0, 10);
+  const [dateAvailability, setDateAvailability] = useState(currentDate);
+
+  const temp = [
+    {
+      time: '18:00',
+      date: '2024-04-04',
+    },
+  ];
+
+  useEffect(() => {
+    listTables();
+  }, [dateAvailability]);
+
   const checkAvailability = (date) => {
-    console.log('checking date', date);
+    setDateAvailability(date);
+  };
+
+  const listTables = () => {
+    console.log('listing tables');
   };
 
   return (
     <div className="booking-app">
       <form action="" className="booking-form">
         <CalendarApp checkAvailability={checkAvailability} />
+        <div className="tables">
+          <h3>Available tables: {dateAvailability}</h3>
+          <ul className="tables-list">
+            <li>Table</li>
+            <li>Table</li>
+            <li>Table</li>
+            <li>Table</li>
+            <li>Table</li>
+          </ul>
+        </div>
       </form>
     </div>
   );
