13baea800db2f5ded626a5f1bb0157f85268ac13
Dalibor Babić <124865552+laddeeth@users.noreply.github.com>
2024-04-05 10:58:34 +0200

    feat: connected buttons to Adminpage to testout Blockchain-Service.js

--- therestaurant/src/Blockchain-Service.js
+++ therestaurant/src/Blockchain-Service.js
@@ -1,64 +1,74 @@
-import { ethers } from 'ethers'
+import { ethers } from 'ethers';
 import { url, adress, abi } from './config';
 
 const provider = new ethers.providers.JsonRpcProvider(url);
 
-const contract = new ethers.Contract(adress, abi, provider);
+const signer = provider.getSigner();
+
+const contract = new ethers.Contract(adress, abi, signer);
 
 async function createRestaurant(name) {
-    try {
-        const tx = await contract.createRestaurant(name)
-        await tx.wait()
-    } catch (error) {
-        console.error('Could not create restaurant: ', error);
-    }
+  try {
+    const tx = await contract.createRestaurant(name);
+    await tx.wait();
+    console.log(tx);
+  } catch (error) {
+    console.error('Could not create restaurant: ', error);
+  }
 }
 
 async function getBookingCount() {
-    try {
-        const count = await contract.bookingCount()
-        return count
-    } catch (error) {
-        console.error('Error getting number of bookings: ', error);
-    }
+  try {
+    const count = await contract.bookingCount();
+    return count;
+  } catch (error) {
+    console.error('Error getting number of bookings: ', error);
+  }
 }
 
 async function getBooking(id) {
-    try {
-        const booking = await contract.bookings(id)
-        return booking
-        
-    } catch (error) {
-        console.error('Could not get the booking: ', error);
-    }
+  try {
+    const booking = await contract.bookings(id);
+    return booking;
+  } catch (error) {
+    console.error('Could not get the booking: ', error);
+  }
 }
 
 async function createBooking(numberOfGuests, name, date, time, restaurantId) {
-    try {
-      const tx = await contract.createBooking(numberOfGuests, name, date, time, restaurantId);
-      await tx.wait();
-      console.log('Booking Created: ', name);
-    } catch (error) {
-      console.error('Error creating booking: ', error);
-    }
+  try {
+    const tx = await contract.createBooking(
+      numberOfGuests,
+      name,
+      date,
+      time,
+      restaurantId
+    );
+    await tx.wait();
+    console.log('Booking Created: ', name);
+  } catch (error) {
+    console.error('Error creating booking: ', error);
   }
+}
 
-  async function removeBooking(id) {
-    try {
-      const tx = await contract.removeBooking(id);
-      await tx.wait();
-      console.log('Booking with id ', id, ' has been removed.');
-    } catch (error) {
-      console.error('Could not remove booking: ', error);
-    }
+async function removeBooking(id) {
+  try {
+    const tx = await contract.removeBooking(id);
+    await tx.wait();
+    console.log('Booking with id ', id, ' has been removed.');
+  } catch (error) {
+    console.error('Could not remove booking: ', error);
   }
+}
 
-  async function updateBooking(id, numberOfGuests, name, date, time) {
-    try {
-      const tx = await contract.editBooking(id, numberOfGuests, name, date, time);
-      await tx.wait();
-      console.log('Booking with id ', id, 'has been updated');
-    } catch (error) {
-      console.error('Could not update booking: ', error);
-    }
+async function updateBooking(id, numberOfGuests, name, date, time) {
+  try {
+    const tx = await contract.editBooking(id, numberOfGuests, name, date, time);
+    await tx.wait();
+    console.log('Booking with id ', id, 'has been updated');
+  } catch (error) {
+    console.error('Could not update booking: ', error);
   }
+}
+
+export { createRestaurant, createBooking, getBookingCount, getBooking };
--- therestaurant/src/pages/Admin.jsx
+++ therestaurant/src/pages/Admin.jsx
@@ -1,10 +1,49 @@
 import { useContext } from 'react';
 import { AdminContext } from '../contexts/AdminContext';
+import {
+  createRestaurant,
+  createBooking,
+  getBookingCount,
+  getBooking,
+} from '../Blockchain-Service';
 
 const Admin = () => {
   const { isAdmin } = useContext(AdminContext);
   if (!isAdmin) return "You don't have permission to view this page";
-  return <div>You are Admin</div>;
+  return (
+    <div>
+      <button onClick={async () => await createRestaurant('Test')}>
+        Create Restaurant
+      </button>
+      <button
+        onClick={async () =>
+          await createBooking(2, 'Dalle', '20240405', 1900, 10)
+        }
+      >
+        Create Booking
+      </button>
+      <button
+        onClick={async () => {
+          const count = await getBookingCount();
+          const decimal = parseInt(count._hex);
+          console.log(count._hex, decimal);
+        }}
+      >
+        Get Booking Count
+      </button>
+      <button
+        onClick={async () => {
+          const count = await getBookingCount();
+          const decimal = parseInt(count._hex);
+          for (let i = 1; i < decimal + 1; i++) {
+            console.log(await getBooking(i));
+          }
+        }}
+      >
+        Get All Bookings
+      </button>
+    </div>
+  );
 };
 
 export default Admin;
