b21623a68d9712434a1f80f653285ec756d570d1
Hugo Barlind <hb223hu@student.lnu.se>
2024-04-09 16:04:31 +0200

    Delete booking when delete button is clicked

--- therestaurant/src/components/BookingList.jsx
+++ therestaurant/src/components/BookingList.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { removeBooking } from "../Blockchain-Service";
 
 const BookingList = ({ data }) => {
   return (
@@ -12,22 +13,30 @@ const BookingList = ({ data }) => {
         <p>Delete</p>
         <p>Update</p>
       </div>
-      {data.map((booking, index) => (
-        <div
-          key={index}
-          className='booking-div'
-        >
-          <p>{booking.name}</p>
-          <p>{booking.date}</p>
-          <p>{parseInt(booking.numberOfGuests._hex, 16)}</p>
-          <p>{parseInt(booking.time._hex, 16)}</p>
-          <p>{parseInt(booking.id._hex, 16)}</p>
-          <button id='delete-btn'>Delete</button>
-          <Link to={`/admin/${parseInt(booking.id._hex, 16)}`}>
-            Edit Booking
-          </Link>
-        </div>
-      ))}
+      {data.map((booking, index) => {
+        if (parseInt(booking.id._hex) == '') return;
+        else {
+          return (
+            <div
+              key={index}
+              className='booking-div'
+            >
+              <p>{booking.name}</p>
+              <p>{booking.date}</p>
+              <p>{parseInt(booking.numberOfGuests._hex, 16)}</p>
+              <p>{parseInt(booking.time._hex, 16)}</p>
+              <p>{parseInt(booking.id._hex, 16)}</p>
+              <button id='delete-btn' onClick={async () => {
+                const res = await removeBooking(parseInt(booking.id._hex, 16))
+                console.log(res);
+              }}>Delete</button>
+              <Link to={`/admin/${parseInt(booking.id._hex, 16)}`}>
+                Edit Booking
+              </Link>
+            </div>
+      )}
+        
+      })}
     </div>
   );
 };
